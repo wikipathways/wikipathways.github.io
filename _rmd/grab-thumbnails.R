6a306ef8cad75f36f71e914bb10aafa431828eb4
Alexander Pico <apico@gladstone.ucsf.edu>
2022-08-29 17:29:41 -0700

    resize png code

--- _rmd/grab-thumbnails.R
+++ _rmd/grab-thumbnails.R
@@ -17,3 +17,18 @@ sapply(approved.wpid, function(p){
   y <- EBImage::resize(x, w = 480) # 3 x display at 160px width
   EBImage::writeImage(y,p.file)
 })
+
+
+## Resize PNGs
+# e.g., downsize all PNGs for storage efficiency
+setwd("~/git/wikipathways/wikipathways-assets/pathways")
+
+dir.ls = list.dirs(full.names = F, recursive = F)
+
+sapply(dir.ls, function(p){
+  sprintf("working on %s",p)
+  p.file = file.path(p,paste(p,"png", sep = "."))
+  x = EBImage::readImage(p.file, type="png")
+  y = EBImage::resize(x, w = 2400)
+  EBImage::writeImage(y,p.file)
+})
